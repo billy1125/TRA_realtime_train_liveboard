e336cfbfc0d5ce2c4a12c34dc822db861656ee48
billy1125 <billy1125@gmail.com>
2023-07-01 20:55:22 +0800

    2023.07.01 更新

--- batch.py
+++ batch.py
@@ -6,19 +6,20 @@ import string
 
 data = getJson.read()
 
-with open('data.json', 'w') as outfile:
+# print(data)
+
+with open(r'C:\xampp\htdocs\data\realtime_trains.json', 'w') as outfile:
     json.dump(data, outfile)
 
+# def del_files(dir,topdown=True):
+#     for root, dirs, files in os.walk(dir, topdown):
+#         for name in files:
+#             pathname = os.path.splitext(os.path.join(root, name))
+#             if (pathname[1] == ".json"):
+#                 # .remove(os.path.join(root, name))
+#                 print(pathname[0])
+#                 shutil.copy(pathname[0]+ pathname[1], 'D:')
 
-def del_files(dir,topdown=True):
-    for root, dirs, files in os.walk(dir, topdown):
-        for name in files:
-            pathname = os.path.splitext(os.path.join(root, name))
-            if (pathname[1] == ".json"):
-                # .remove(os.path.join(root, name))
-                print(pathname[0])
-                shutil.copy(pathname[0]+ pathname[1], '/Library/WebServer/Documents/tradiagram/liveboard')
-
-dir = os.getcwd() 
-print(dir)
-del_files(dir) 
+# dir = os.getcwd() 
+# print(dir)
+# del_files(dir) 
--- read_JSON_URL.py
+++ read_JSON_URL.py
@@ -6,11 +6,11 @@ from time import mktime
 import base64
 from requests import request
 from pprint import pprint
-
 import json
 
 app_id = 'd3901f01fc584f2990799e6b6cd38086'
-app_key = 'u19eifzTL99aUjr35g6rwDdi1RM'
+app_key = '_1WRKyDUrjWZVfVGtHuyQQujdzk'
+
 
 class Auth():
 
@@ -20,7 +20,8 @@ class Auth():
 
     def get_auth_header(self):
         xdate = format_date_time(mktime(datetime.now().timetuple()))
-        hashed = hmac.new(self.app_key.encode('utf8'), ('x-date: ' + xdate).encode('utf8'), sha1)
+        hashed = hmac.new(self.app_key.encode('utf8'),
+                          ('x-date: ' + xdate).encode('utf8'), sha1)
         signature = base64.b64encode(hashed.digest()).decode()
 
         authorization = 'hmac username="' + self.app_id + '", ' + \
@@ -30,14 +31,13 @@ class Auth():
         return {
             'Authorization': authorization,
             'x-date': format_date_time(mktime(datetime.now().timetuple())),
-            'Accept - Encoding': 'gzip'
+            'Accept-Encoding': 'gzip'
         }
 
 
 def read():
     a = Auth(app_id, app_key)
-    response = request('get', 'http://ptx.transportdata.tw/MOTC/v2/Rail/TRA/LiveBoard?$format=JSON', headers= a.get_auth_header())
-
+    response = request('get', 'https://ptx.transportdata.tw/MOTC/v3/Rail/TRA/TrainLiveBoard?$format=JSON', headers=a.get_auth_header())
     data = json.loads(response.content.decode('utf-8'))
-
     return data
+    # pprint(response.content)
